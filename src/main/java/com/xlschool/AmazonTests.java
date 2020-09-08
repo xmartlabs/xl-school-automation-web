5f9dec9adb39a8a0aafd5b1997fb2d2eb5f98b33
nachosecco <nachosecco@gmail.com>
2020-09-08 08:25:42 -0300

    Added DemoQATests Class

--- src/main/java/com/xlschool/AmazonTests.java
+++ src/main/java/com/xlschool/AmazonTests.java
@@ -1,36 +1,40 @@
 package com.xlschool;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class AmazonTests {
-    public static void amazonTest1(){
-    }
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+
         driver.get("https://amazon.com");
         waiter(3000);
+
         System.out.println("El titulo de la pagina es: " + driver.getTitle());
         WebElement inputSearch = driver.findElement(By.xpath("//*[@id='twotabsearchtextbox']"));
+        highlighElement(driver,inputSearch);
         inputSearch.sendKeys("aladdin");
-        waiter(2000);
-        WebElement buttonSearch = driver.findElement(By.id("nav-search-submit-text"));
-        buttonSearch.click();
+        waiter(3000);
 
+        WebElement buttonSearch = driver.findElement(By.id("nav-search-submit-text"));
+        highlighElement(driver,buttonSearch);
+        buttonSearch.click();
         waiter(5000);
 
         WebElement storesDropDown = driver.findElement(By.id("searchDropdownBox"));
+        highlighElement(driver,storesDropDown);
+        waiter(2000);
         storesDropDown.click();
         Select storeSelector = new Select(storesDropDown);
         //storeSelector.deselectByValue("search-alias=warehouse-deals");
         storeSelector.selectByVisibleText("Books");
-
         waiter(5000);
 
         driver.close();
@@ -43,6 +47,12 @@ public class AmazonTests {
         } catch (Exception e) {
 
         }
+    }
+
+
+    public static void highlighElement(WebDriver driver, WebElement element) {
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("arguments[0].style.backgroundColor = '#FDFF47';", element);
     }
 
 }

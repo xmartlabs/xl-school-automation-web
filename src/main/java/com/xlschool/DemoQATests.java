c8998834c54e9af3e78f981e07c1b981aa20b9f7
nachosecco <nachosecco@gmail.com>
2020-09-08 09:29:22 -0300

    Fixed DemoQATests class

--- src/main/java/com/xlschool/DemoQATests.java
+++ src/main/java/com/xlschool/DemoQATests.java
@@ -1,65 +1,8 @@
 package com.xlschool;
 
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class DemoQATests {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-
-        driver.get("https://amazon.com");
-        waiter(3000);
-    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    private static void waiter(int pTime) {
-        try {
-            Thread.sleep(pTime);
-        } catch (Exception e) {
-
-        }
-    }
-
-    public static void highlighElement(WebDriver driver, WebElement element) {
-        JavascriptExecutor jse = (JavascriptExecutor) driver;
-        jse.executeScript("arguments[0].style.backgroundColor = '#FDFF47';", element);
     }
 }

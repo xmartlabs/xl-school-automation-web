54238d2f85b2ab459fc7679ae4af604e64d60d24
Jorge Gonzalez Albisu <jorge.gonzalez.albisu@gmail.com>
2020-09-15 11:06:30 -0300

    20200915 - Adding the last code of today class

--- src/main/java/com/xlschool/DemoQATests.java
+++ src/main/java/com/xlschool/DemoQATests.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class DemoQATests {
     public static void main(String[] args) {
@@ -29,21 +30,86 @@ public class DemoQATests {
          */
 
 
+
+
         //Step 1
         System.setProperty("webdriver.chrome.driver", "chromedriver");
         WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
 
-        driver.get("https://demoqa.com/");
-        waiter(3000);
+        try {
 
-        //Step 2
-        WebElement divForms = driver.findElement(By.xpath("//*[@class='category-cards']/div[contains(string(),/Forms/)]"));
-        divForms.click();
-        waiter(2000);
+            driver.manage().window().maximize();
 
-        //Step 3
-        WebElement spanForm = driver.findElement(By.xpath("//span[contains(string(),'Practice Form')]"));
+            driver.get("https://demoqa.com/");
+            waiter(3000);
+
+            //Step 2
+            WebElement divForms = driver.findElement(By.xpath("//*[@class='category-cards']/div[contains(string(),'Forms')]"));
+            divForms.click();
+            waiter(2000);
+
+            //Step 3
+            WebElement spanForm = driver.findElement(By.xpath("//span[contains(string(),'Practice Form')]"));
+            spanForm.click();
+
+            //Step 4 (Assert Student Registration Form)
+            waiter(4000);
+            WebElement headerStudentRegistrationForm  = driver.findElement(By.xpath("//div[@class='practice-form-wrapper']//h5[contains(text(), 'Registration')]"));
+            String headerStudentRegistrationFormActualText = headerStudentRegistrationForm.getText();
+            System.out.println("headerStudentRegistrationFormText = " + headerStudentRegistrationFormActualText);
+
+//            String headerStudentRegistrationFormText2 = headerStudentRegistrationForm.getAttribute("text");
+//            System.out.println("headerStudentRegistrationFormText2 = " + headerStudentRegistrationFormText2);
+
+            String headerStudentRegistrationFormExpectedText = "Student Registration Form";
+            String resultado = "";
+
+            if (headerStudentRegistrationFormActualText.equals(headerStudentRegistrationFormExpectedText) == true ){
+                resultado = "Coincide";
+            }else if (headerStudentRegistrationFormActualText.equals(headerStudentRegistrationFormExpectedText) == false){
+                resultado = "No coincide y tamo leyendo cualquiera";
+            }
+
+            System.out.println("El texto del título coincide?" + resultado);
+
+
+            //Step 5 (Clickear submit)
+            waiter(4000);
+            WebElement buttonSubmitStudentRegistrationForm = driver.findElement(By.id("submit"));
+
+
+            // Acá se programa un actions para poder hacer scroll primero al elemento, para que sea visible, y posteriormente hacerle click.
+            Actions action = new Actions(driver);
+            action.moveToElement(buttonSubmitStudentRegistrationForm).build().perform();
+            buttonSubmitStudentRegistrationForm.click();
+
+
+
+//            6- Validar que sigo en student registration form
+//            7- Entrar letras en el mobile
+//            8- Assert que sigue rojo
+//            9- Entrar 10 dígitos
+//            10- Assert tick
+//            11- Entrar información en todos los campos (first name, last name, email, gender, mobile, DOB, subjects, hobbies, state and city).
+//            12- Click en submit
+//            13- Assert Thanks for submitting the form
+//            14- Validar que los valores sean correctos (guardar los valores que iba entrando en variables y usar esas variables)
+
+
+
+        }catch (Exception e) {
+            System.out.println("Se rompio" );
+            e.printStackTrace();
+        }finally {
+
+
+            driver.close();
+            driver.quit();
+        }
+
+
+
+
 
 
         waiter(4000);

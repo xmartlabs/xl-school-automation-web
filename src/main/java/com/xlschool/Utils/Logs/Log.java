7d63764d7508aa8add13c85ea7fb3b974496a2ae
Ignacio Secco <isecco@guardanthealth.com>
2020-10-08 14:50:26 -0300

    Add Log and logback config

--- src/main/java/com/xlschool/Utils/Logs/Log.java
+++ src/main/java/com/xlschool/Utils/Logs/Log.java
@@ -1,19 +1,22 @@
 package com.xlschool.Utils.Logs;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Log {
 
     private static Logger log;
 
+    private static void setLog(String pName){
+        log = LoggerFactory.getLogger(pName);
+    }
+
 
     // This is to print log for the beginning of the test case, as we usually run so many test cases as a test suite
 
-    public static void startTestCase(Class pClass, String sTestCaseName){
+    public static void startTestCase(String pName, String sTestCaseName){
 
-        log = LogManager.getLogger(pClass);
+        log = LoggerFactory.getLogger(pName);
 
         log.info("****************************************************************************************");
 
@@ -29,9 +32,9 @@ public class Log {
 
     //This is to print log for the ending of the test case
 
-    public static void endTestCase(Class pClass, String sTestCaseName){
+    public static void endTestCase(String pName){
 
-        log = LogManager.getLogger(pClass);
+        setLog(pName);
 
         log.info("XXXXXXXXXXXXXXXXXXXXXXX             "+"-E---N---D-"+"             XXXXXXXXXXXXXXXXXXXXXX");
 
@@ -47,36 +50,28 @@ public class Log {
 
     // Need to create these methods, so that they can be called
 
-    public static void info(Class pClass, String message) {
-        log = LogManager.getLogger(pClass);
-
+    public static void info(String pName, String message) {
+        setLog(pName);
         log.info(message);
-
     }
 
-    public static void warn(Class pClass, String message) {
-        log = LogManager.getLogger(pClass);
-
+    public static void warn(String pName, String message) {
+        setLog(pName);
         log.warn(message);
-
     }
 
-    public static void error(Class pClass, String message) {
-        log = LogManager.getLogger(pClass);
-
+    public static void error(String pName, String message) {
+        setLog(pName);
         log.error(message);
-
     }
 
-    public static void fatal(Class pClass, String message) {
-        log = LogManager.getLogger(pClass);
-
-        log.fatal(message);
-
+    public static void trace(String pName, String message) {
+        setLog(pName);
+        log.trace(message);
     }
 
-    public static void debug(Class pClass, String message) {
-        log = LogManager.getLogger(pClass);
+    public static void debug(String pName, String message) {
+        setLog(pName);
 
         log.debug(message);
 
